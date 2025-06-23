0fb87d61bc7f5cbcf3581ba0bf08187106bd13ed
isacpxc <isacpxc@gmail.com>
2025-06-23 10:38:39 -0300

    feat: improve endpoint added

--- server/server.py
+++ server/server.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import Response
+from flask import request
+import json
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.post("/improve")
+def improve_text():
+    res = request.get_json()
+    json_res = {"received_Text": res["text_to_improve"]}
+    return json_res
