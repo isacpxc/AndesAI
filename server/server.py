2b20c151f3c44de879d1c2d4a55a347dd2b41d01
isacpxc <isacpxc@gmail.com>
2025-06-23 18:41:27 -0300

    feat: try/except and updated code

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 import requests
 import json
@@ -13,15 +13,30 @@ def hello_world():
 
 @app.post("/improve")
 def improve_text():
-    req = request.get_json()
-    text_to_improve = req['text_to_improve']
-    json_res = {
+    try:
+        data = request.get_json()
+        text_to_improve = data['text_to_improve']
+    except Exception as e:
+        return jsonify({"error": "JSON inválido ou faltando 'text_to_improve'"}), 400
+    
+    ollama_payload = {
         "model": "llama3.1:8b", 
         "prompt": f"Correct the following text based on the grammar and semantics of used language. Only return the corrected and improved text, without adding comments, explanations or observations. The result must contain only the revised text. Text: {text_to_improve}",
         "stream": False}
+    try:
+        response = requests.post(url=url, json=ollama_payload)
+        response.raise_for_status()
+        
+        ollama_data = response.json()
+        improved_text = ollama_data.get("response", "")
+        
+        return jsonify({"improved_text": improved_text})
+        
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Error comunicating with ollama: {e}"}), 503 
+    except Exception as e:
+        return jsonify({"error": f"Unexpected Server Error: {e}"}), 500
+        
     
-    res = requests.post(url=url, json=json_res)
-    res = json.loads(res.text)
-    res = res["response"]
-    
-    return res
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
